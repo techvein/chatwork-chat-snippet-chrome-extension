6a85c967ed6574aaa9a2ad2905f5f219504b2985
InomataMitsuhiro <mi@calvados.local>
2015-07-15 19:51:17 +0900

    jquery を使わないようにした。

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
 (function(){
 // document.addEventListener("DOMContentLoaded", function(e){
-    console.log('chatwork snippets loaded');
-    var $chatSendTool = $('#_chatSendTool');
-    if($chatSendTool.size() == 0){ return; }
+  console.log('chatwork snippets loaded');
+  var chatSendTool = document.querySelector('#_chatSendTool');
+  if(!chatSendTool){ return; }
 
-    var $code = $('<li id="_techvein_codeIcon" role="button" class="_showDescription icoFont" aria-label="[code]タグ埋め込み">[code]</li>'
-		  );
-    $chatSendTool.append($code);
+  var code = document.createElement('li');
+  code.setAttribute('id', '_techvein_codeIcon');
+  code.setAttribute('role', 'button');
+  code.setAttribute('class', '_showDescription icoFont');
+  code.setAttribute('aria-label', '[code]タグ埋め込み');
+  code.appendChild(document.createTextNode('[code]'));
+  chatSendTool.appendChild(code);
 
-    $code.on('click', function(){
-	    var $chatText = $('#_chatText');
-	    replaceTextArea($chatText,function(text){ 
+  code.addEventListener('click', function(){
+	    var chatText = document.querySelector('#_chatText');
+	    replaceTextArea(chatText,function(text){
 		    return '[code]' + text + '[/code]';
+		  });
+ 	});
+
+  var info = document.createElement('li');
+  info.setAttribute('id', '_techvein_infoIcon');
+  info.setAttribute('role', 'button');
+  info.setAttribute('class', '_showDescription icoFont');
+  info.setAttribute('aria-label', '[info]タグ埋め込み');
+  info.appendChild(document.createTextNode('[info]'));
+  chatSendTool.appendChild(info);
+
+  info.addEventListener('click', function(){
+    var chatText = document.querySelector('#_chatText');
+    replaceTextArea(chatText,function(text){
+	    return '[info]' + text + '[/info]';
 		});
 	});
-
-    var $info = $('<li id="_techvein_codeIcon" role="button" class="_showDescription icoFont" aria-label="[info]タグ埋め込み">[info]</li>'
-		  );
-    $chatSendTool.append($code);
-
-    $info.on('click', function(){
-	    var $chatText = $('#_chatText');
-	    replaceTextArea($chatText,function(text){ 
-		    return '[info]' + text + '[/info]';
-		});
-	});
-  
-    function replaceTextArea($textArea, filterSelectedText){
-	var textArea = $textArea.get(0);
-	if(textArea == undefined){ 
-	    console.log('missing textArea');
-	    return; 
-	}
-	textArea.focus(); 
-	var content = textArea.value;
-	var start = textArea.selectionStart;
-	var end = textArea.selectionEnd;
-	var preText = content.substr(0, start);
-	var postText = content.substr(end);
-	var selectedText = content.substr(start, end - start);
-	var res = preText + filterSelectedText(selectedText) + postText;
-	textArea.value = res;
-    }
+  function replaceTextArea(textArea, filterSelectedText){
+  	if(!textArea){
+  	    console.log('missing textArea');
+  	    return;
+  	}
+  	textArea.focus();
+  	var content = textArea.value;
+  	var start = textArea.selectionStart;
+  	var end = textArea.selectionEnd;
+  	var preText = content.substr(0, start);
+  	var postText = content.substr(end);
+  	var selectedText = content.substr(start, end - start);
+  	var res = preText + filterSelectedText(selectedText) + postText;
+  	textArea.value = res;
+  }
 })();
