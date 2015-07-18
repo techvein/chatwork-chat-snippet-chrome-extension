fdfc8afd855eee50c43f61762b441d4b481862dd
InomataMitsuhiro <mi@calvados.local>
2015-07-18 16:21:21 +0900

    infoにタイトル追加＆hr追加

--- src/main.js
+++ src/main.js
@@ -4,63 +4,6 @@
   var chatSendTool = document.querySelector('#_chatSendTool');
   if(!chatSendTool){ return; }
 
-  var code = document.createElement('li');
-  code.setAttribute('id', '_techvein_codeIcon');
-  code.setAttribute('role', 'button');
-  code.setAttribute('class', '_showDescription icoFont');
-  code.setAttribute('aria-label', '[code]タグ埋め込み');
-  code.appendChild(document.createTextNode('[code]'));
-  chatSendTool.appendChild(code);
-
-  code.addEventListener('click', function(){
-    var chatText = document.querySelector('#_chatText');
-    replaceTextArea(chatText,function(texts){
-      var prefix = '[code]';
-      var postfix = '[/code]';
-
-      // 改行を足す(重複チェックあり)
-      if(texts.selectedText.substr(0,1) != '\n'){
-        prefix += '\n'; // codeは１行目の改行をスキップしてくれる。
-      }
-      if(texts.selectedText.slice(-1) != '\n'){ // last char
-        postfix = '\n' + postfix;
-      }
-      if(texts.postText.substr(0,1) != '\n'){
-        postfix += '\n';
-      }
-      texts.preText +=  prefix;
-      texts.postText = postfix + texts.postText;
-      return texts;
-	  });
- 	});
-
-  var info = document.createElement('li');
-  info.setAttribute('id', '_techvein_infoIcon');
-  info.setAttribute('role', 'button');
-  info.setAttribute('class', '_showDescription icoFont');
-  info.setAttribute('aria-label', '[info]タグ埋め込み');
-  info.appendChild(document.createTextNode('[info]'));
-  chatSendTool.appendChild(info);
-
-
-  info.addEventListener('click', function(){
-    var chatText = document.querySelector('#_chatText');
-    replaceTextArea(chatText,function(texts){
-      var prefix = '[info]';
-      var postfix = '[/info]';
-
-      // 改行を足す(重複チェックあり)
-      if(texts.selectedText.slice(-1) != '\n'){ // last char
-        postfix = '\n' + postfix;
-      }
-      if(texts.postText.substr(0,1) != '\n'){
-        postfix += '\n';
-      }
-      texts.preText +=  prefix;
-      texts.postText = postfix + texts.postText;
-      return texts;
-	  });
-	});
     function replaceTextArea(textArea, filter){
     	if(!textArea){
     	    console.log('missing textArea');
@@ -90,4 +33,112 @@
       textArea.selectionEnd = texts.preText.length + texts.selectedText.length;
 
     }
+    function createIcon(iconNode, description, filterFunc){
+      var code = document.createElement('li');
+      code.setAttribute('id', '_techvein_codeIcon');
+      code.setAttribute('role', 'button');
+      code.setAttribute('class', '_showDescription icoFont');
+      code.setAttribute('aria-label', '[code]タグ埋め込み');
+      code.appendChild(document.createTextNode('[code]'));
+      chatSendTool.appendChild(code);
+
+      code.addEventListener('click', function(){
+        var chatText = document.querySelector('#_chatText');
+        replaceTextArea(chatText,function(texts){
+          var prefix = '[code]';
+          var postfix = '[/code]';
+
+          // 改行を足す(重複チェックあり)
+          if(texts.selectedText.substr(0,1) != '\n'){
+            prefix += '\n'; // codeは１行目の改行をスキップしてくれる。
+          }
+          if(texts.selectedText.slice(-1) != '\n'){ // last char
+            postfix = '\n' + postfix;
+          }
+          if(texts.postText.substr(0,1) != '\n'){
+            postfix += '\n';
+          }
+          texts.preText +=  prefix;
+          texts.postText = postfix + texts.postText;
+          return texts;
+    	  });
+     	});
+    }
+
+    var code = document.createElement('li');
+    code.setAttribute('id', '_techvein_codeIcon');
+    code.setAttribute('role', 'button');
+    code.setAttribute('class', '_showDescription icoFont');
+    code.setAttribute('aria-label', '[code]タグ埋め込み');
+    code.appendChild(document.createTextNode('[code]'));
+    chatSendTool.appendChild(code);
+
+    code.addEventListener('click', function(){
+      var chatText = document.querySelector('#_chatText');
+      replaceTextArea(chatText,function(texts){
+        var prefix = '[code]';
+        var postfix = '[/code]';
+
+        // 改行を足す(重複チェックあり)
+        if(texts.selectedText.substr(0,1) != '\n'){
+          prefix += '\n'; // codeは１行目の改行をスキップしてくれる。
+        }
+        if(texts.selectedText.slice(-1) != '\n'){ // last char
+          postfix = '\n' + postfix;
+        }
+        if(texts.postText.substr(0,1) != '\n'){
+          postfix += '\n';
+        }
+        texts.preText +=  prefix;
+        texts.postText = postfix + texts.postText;
+        return texts;
+  	  });
+   	});
+
+    var info = document.createElement('li');
+    info.setAttribute('id', '_techvein_infoIcon');
+    info.setAttribute('role', 'button');
+    info.setAttribute('class', '_showDescription icoFont');
+    info.setAttribute('aria-label', '[info]タグ埋め込み');
+    info.appendChild(document.createTextNode('[info]'));
+    chatSendTool.appendChild(info);
+
+
+    info.addEventListener('click', function(){
+      var chatText = document.querySelector('#_chatText');
+      replaceTextArea(chatText,function(texts){
+        var prefix = '[info]';
+        var postfix = '[/info]';
+
+        // 改行を足す(重複チェックあり)
+        if(texts.selectedText.slice(-1) != '\n'){ // last char
+          postfix = '\n' + postfix;
+        }
+        if(texts.postText.substr(0,1) != '\n'){
+          postfix += '\n';
+        }
+        texts.preText +=  prefix;
+        texts.selectedText = '[title][/title]' + texts.selectedText;
+        texts.postText = postfix + texts.postText;
+        return texts;
+  	  });
+  	});
+
+    var hr = document.createElement('li');
+    hr.setAttribute('id', '_techvein_infoIcon');
+    hr.setAttribute('role', 'button');
+    hr.setAttribute('class', '_showDescription icoFont');
+    hr.setAttribute('aria-label', '[hr]タグ埋め込み');
+    hr.appendChild(document.createTextNode('[hr]'));
+    chatSendTool.appendChild(hr);
+
+
+    hr.addEventListener('click', function(){
+      var chatText = document.querySelector('#_chatText');
+      replaceTextArea(chatText,function(texts){
+        texts.preText += '[hr]';
+        return texts;
+  	  });
+  	});
+
 })();
